bfe754e24a986c07dce3406f4dcf373bb0ab777d
Osman-Hashem <osmanhashem461@gmail.com>
2024-12-17 00:24:59 +0200

    Solve All Ass

---
+++ Ass - 2/Program.cs
@@ -0,0 +1,171 @@
+﻿namespace Ass___2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            #region Q01 :  Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no
+
+
+            // Console.WriteLine("Enter The Number : ");
+
+            // int Number = int.Parse(Console.ReadLine());
+            // 
+            // 
+            // 
+            // if (Number % 3 == 0 && Number % 4 == 0)
+            // {
+            //     Console.WriteLine("Yes ");
+            // }
+            // else 
+            // {
+            //     Console.WriteLine("No");
+            // }
+
+            #endregion
+
+            #region Q02 : Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.
+
+            // Console.WriteLine("Enter The Number : ");
+            // 
+            // int Number = int.Parse(Console.ReadLine());
+            // 
+            // 
+            // 
+            // if (Number < 0 )
+            // {
+            //     Console.WriteLine("Negative ");
+            // }
+            // else 
+            // {
+            //     Console.WriteLine("Positive ");
+            // }
+
+
+
+            #endregion
+
+            #region Q03 : Write a program that takes 3 integers from the user then prints the max element and the min element.
+
+            // Console.WriteLine("Enter 3 Numbers : ");
+            // 
+            // int Number01 = int.Parse(Console.ReadLine());
+            // int Number02 = int.Parse(Console.ReadLine());
+            // int Number03 = int.Parse(Console.ReadLine());
+            // 
+            // 
+            // 
+            // if (Number01 > Number02 && Number01 > Number03)
+            // {
+            //     Console.WriteLine(" Max Element : " + Number01);
+            // }
+            // else if (Number02 > Number03 && Number02 > Number01)
+            // {
+            //     Console.WriteLine(" Max Element : " + Number02);
+            // }
+            // else
+            // {
+            //     Console.WriteLine(" Max Element : " + Number03);
+            // }
+            // 
+            // Console.WriteLine();
+            // 
+            // if (Number01 < Number02 && Number01 < Number02)
+            // {
+            //     Console.WriteLine(" Min Element : " + Number01);
+            // }
+            // else if (Number02 < Number03 && Number02 < Number01)
+            // {
+            //     Console.WriteLine(" Min Element : " + Number02);
+            // }
+            // else
+            // {
+            //     Console.WriteLine(" Min Element : " + Number03);
+            // }
+
+
+
+
+            #endregion
+
+            #region Q04 : Write a program that allows the user to insert an integer number then check If a number is even or odd.
+
+            // Console.WriteLine("Enter the Number : ");
+            // 
+            // int Number = int.Parse(Console.ReadLine());
+            // 
+            // if (Number % 2 == 0)
+            // {
+            //     Console.WriteLine(" Even Number ");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Odd Number ");
+            // }
+            #endregion
+
+            #region Q05 : Write a program that takes character from the user then if it is a vowel chars (a,e,I,o,u) then print (vowel) otherwise print (consonant) ***
+
+
+
+            #endregion
+
+            #region Q06 : Write a program that allows the user to insert an integer then print all numbers between 1 to that number. ***
+
+            Console.WriteLine("Enter The Number : ");
+
+            // int Number = int.Parse(Console.ReadLine());
+            // 
+            // do 
+            // {
+            //     Console.WriteLine(Number);
+            // 
+            // }
+            // while (Number >= 1);
+            #endregion
+
+            #region Q07 : Write a program that allows the user to insert an integer then print a multiplication table up to 12.***
+
+ 
+
+            #endregion
+
+            #region Q08 : Write a program that allows to user to insert number then print all even numbers between 1 to this number ***
+
+
+
+            #endregion
+
+            #region Q09 : Write a program that takes two integers then prints the power. ***
+
+
+        #endregion
+
+            #region Q10 : Write a program to enter marks of five subjects and calculate total, average and percentage. ***
+
+        #endregion
+
+            #region Q11 : Write a program to input the month number and print the number of days in that month ***
+
+        #endregion
+
+            #region Q12 : Write a program to create a Simple Calculator *** 
+
+        #endregion
+
+            #region Q13 : Write a program to allow the user to enter a string and print the REVERSE of i ***
+
+        #endregion
+
+            #region Q14 : Write a program to allow the user to enter int and print the REVERSED of it ***
+
+        #endregion
+
+
+
+
+
+        }
+    }
+}
